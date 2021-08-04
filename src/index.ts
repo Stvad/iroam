69fd034489a567e7198aa63f913a67864dd4c54f
Vladyslav Sitalo <sitalov@amazon.com>
2021-08-04 17:33:04 +0200

    Expose roamClient to be more easily accessible from nodes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import { handleKeyPress } from "./keyboard";
+import {handleKeyPress} from "./keyboard"
+import * as roamClient from "roam-client"
+import {Block} from "roam-client"
+
+import './execution-style.css'
 
 console.log("Loading iroam.")
 
-document.addEventListener("keydown", handleKeyPress);
+document.addEventListener("keydown", handleKeyPress)
+
+// Exposing all the utilities to be more easily accessible in the notebook.
+window.roamClient = roamClient
+window.block = Block.fromUid
