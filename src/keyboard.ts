4a762cc5f23617e342787e84628599c315dfa430
Vladyslav Sitalo <sitalov@amazon.com>
2021-08-09 14:19:28 +0200

    Fix bug when event propagation was blocked for all shortcuts

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -5,15 +5,16 @@ import {createNextCodeBlock} from "./api"
 export const handleKeyPress = async (e: KeyboardEvent) => {
     if (e.code === "Enter" && e.metaKey === true && !e.shiftKey) {
         await runActiveBlockAndWriteToNext()
+        e.stopPropagation()
     } else if (e.code === "Enter" && e.altKey === true) {
         await runActiveBlockAndWriteToNext()
         await createNextCodeBlock(getActiveBlockUid())
+        e.stopPropagation()
     } else if (e.code === "Enter" && e.metaKey === true && e.shiftKey === true) {
         runActiveNotebook()
+        e.stopPropagation()
     } else if (e.key === "-" && e.ctrlKey === true && e.metaKey === true) {
         console.log("iroam: Removing key listener")
         document.removeEventListener("keydown", handleKeyPress)
     }
-
-    e.stopPropagation()
 }
